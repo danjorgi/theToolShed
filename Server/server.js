7673505986cd7e82fbace48a8d21c92ffd6b06d7
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-12-12 20:47:06 -0700

    Quick update

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = 4004
+
+app.listen(4004, () => {
+    console.log(`Server is running on port ${port}`);
+});
