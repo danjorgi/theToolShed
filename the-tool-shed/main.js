608f2ed5a9af4687ac1f1b42925200111ad8c701
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-12-15 15:58:23 -0700

    Javascript for adding a tool added.

---
+++ the-tool-shed/main.js
@@ -0,0 +1,39 @@
+const availableSection = document.querySelector('#available-section');
+const form = document.querySelector('#toolAdd');
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let imageURL = document.querySelector('#image').value;
+    let toolName = document.querySelector('#toolName').value;
+    let ownerName = document.querySelector('#ownerName').value;
+
+    let toolObj = {
+        imageURL: imageURL,
+        toolName: toolName,
+        ownerName: ownerName
+    };
+
+    createToolCardAvailable(toolObj);
+
+    document.querySelector('#image').value = '';
+    document.querySelector('#toolName').value = '';
+    document.querySelector('#ownerName').value = '';
+}
+
+function createToolCardAvailable(tool) {
+    const toolCard = document.createElement('div');
+    toolCard.classList.add('toolCardAvailable');
+
+    toolCard.innerHTML = `
+    <h5 class="ownerName">Owner: ${tool.ownerName}</h5>
+    <img class="toolPic" src="${tool.imageURL}" alt="${tool.toolName}">
+    <h4 class="toolName">${tool.toolName}</h4>
+    <button class="borrowBtn">Borrow Tool</button>
+    <button class="deleteBtn">X</button>
+    `
+
+    availableSection.appendChild(toolCard)
+}
+
+form.addEventListener('submit', submitHandler);
