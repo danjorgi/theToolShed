0cb84d482c0543209175924e7a9f0011b86f4241
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-12-19 17:09:04 -0700

    Fixed create tool/delete tool to now work on front and backend.

--- Server/controller.js
+++ Server/controller.js
@@ -1,16 +1,22 @@
 let tools = require("./availableDB.json");
-let globalId = 3;
+let globalId = tools.length > 0 ? Math.max(...tools.map(tool => tool.id)) + 1 : 1;
+
 
 module.exports = {
     getTools: (req, res) => {
-        res.status(200).send(tools)
+        res.status(200).send(tools);
     },
     deleteTool: (req, res) => {
-        let index = tools.findIndex(elem => elem.id === +req.params.id)
-        tools.splice(index, 1)
-        res.status(200).send(tools)
+        let index = tools.findIndex(elem => elem.id === +req.params.id);
+        if (index !== -1 ) {
+            tools.splice(index, 1);
+            res.status(200).send(tools);
+        } else {
+            res.status(404).json({error: 'Tool not found'});
+        }
     },
     createTool: (req, res) => {
+        console.log('Received request:', req.body);
         let {imageURL, toolName, ownerName} = req.body;
         let newTool = {
             id: globalId,
--- the-tool-shed/main.js
+++ the-tool-shed/main.js
@@ -7,11 +7,21 @@ const toolsCallback = ({ data: tools }) => displayAvailableTools(tools);
 const errCallback = err => console.log(err);
 
 const getAllAvailableTools = () => axios.get(`${baseURL}/toolsAvailable`).then(toolsCallback).catch(errCallback);
-const createTool = body => axios.post(`${baseURL}/toolsAvailable`, body).then(toolsCallback).catch(errCallback);
+const createTool = body => axios.post(`${baseURL}/toolsAvailable`, body)
+    .then(response => {
+        const createdTool = response.data;
+        console.log('Tool created successfully:', response.data);
+        toolsCallback(response);
+    })
+    .catch(err => {
+        console.error('Error creating tool:', err);
+        errCallback(err);
+});
 const deleteTool = id => axios.delete(`${baseURL}/toolsAvailable/${id}`).then(toolsCallback).catch(errCallback);
 
 function submitHandler(e) {
     e.preventDefault();
+    console.log('Submit handler called');
 
     let imageURL = document.querySelector('#image').value;
     let toolName = document.querySelector('#toolName').value;
@@ -23,7 +33,7 @@ function submitHandler(e) {
         ownerName: ownerName
     };
 
-    createToolCardAvailable(toolObj);
+    createTool(toolObj);
 
     document.querySelector('#image').value = '';
     document.querySelector('#toolName').value = '';
@@ -31,6 +41,7 @@ function submitHandler(e) {
 }
 
 function createToolCardAvailable(tool) {
+    console.log('Creating tool card:', tool);
     const toolCard = document.createElement('div');
     toolCard.classList.add('toolCardAvailable');
 
@@ -48,7 +59,7 @@ function createToolCardAvailable(tool) {
 
 function displayAvailableTools(arr) {
     availableSection.innerHTML = ``
-    for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         createToolCardAvailable(arr[i])
     }
 }
